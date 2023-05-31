cd773c7ca83af238e2fe648c9311222176194297
materog <131563986+materog@users.noreply.github.com>
2023-05-31 03:07:29 +0200

    Add files via upload

---
+++ Kreiranje baze podataka.sql
@@ -0,0 +1,45 @@
+CREATE TABLE Korisnici (
+  KorisnikID INT IDENTITY(1,1) PRIMARY KEY,
+  KorisnickoIme VARCHAR(50),
+  Lozinka VARCHAR(50),
+  Uloga SMALLINT,
+  Potvrda BIT
+);
+
+CREATE TABLE Profesori (
+  ProfesorID INT IDENTITY(1,1) PRIMARY KEY,
+  KorisnikID INT,
+  Ime VARCHAR(50),
+  Prezime VARCHAR(50),
+  FOREIGN KEY (ProfesorID) REFERENCES Korisnici (KorisnikID)
+);
+
+CREATE TABLE Studenti (
+  StudentID INT IDENTITY(1,1) PRIMARY KEY,
+  KorisnikID INT,
+  Ime VARCHAR(50),
+  Prezime VARCHAR(50),
+  TipStudija SMALLINT,
+  FOREIGN KEY (KorisnikID) REFERENCES Korisnici (KorisnikID)
+);
+
+CREATE TABLE Teme (
+  TemaID INT IDENTITY(1,1) PRIMARY KEY,
+  ProfesorID INT,
+  StudentID INT,
+  Dostupnost BIT,
+  NazivTeme VARCHAR(100),
+  KratkiOpis VARCHAR(500),
+  Studij SMALLINT,
+  FOREIGN KEY (ProfesorID) REFERENCES Profesori (ProfesorID),
+  FOREIGN KEY (StudentID) REFERENCES Studenti (StudentID)
+);
+
+CREATE TABLE Prijave (
+  PrijavaID INT IDENTITY(1,1) PRIMARY KEY,
+  TemaID INT,
+  StudentID INT,
+  Prihvaceno BIT,
+  FOREIGN KEY (TemaID) REFERENCES Teme (TemaID),
+  FOREIGN KEY (StudentID) REFERENCES Studenti (StudentID)
+);
